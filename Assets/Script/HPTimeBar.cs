3164e8c00a4b54c255d33e14800b65412cb499a1
Worldsixthocean <oceanwong524@gmail.com>
2023-10-27 01:39:09 +0800

    Timer add pause function

--- Assets/Script/HPTimeBar.cs
+++ Assets/Script/HPTimeBar.cs
@@ -10,7 +10,8 @@ public class HPTimeBar : MonoBehaviour
     [SerializeField] private float HP;
     [SerializeField] private int MaxHP;
     [SerializeField] private int HPchange;
-    [SerializeField] private int time;
+    [SerializeField] private float time;
+    [SerializeField] private bool timerActive = false;
     [SerializeField] private bool isCountingDown = false;
     [SerializeField] RectTransform _hp;
     [SerializeField] Text _timeText;
@@ -19,45 +20,55 @@ public class HPTimeBar : MonoBehaviour
     {
     }
 
-    public void AddHP(int HPArg) { 
+    public void AddHP(int HPArg = 10) { 
         if (isCountingDown) 
             HP = (HP + HPArg > MaxHP) ? MaxHP : (HP + HPArg);
     }
 
-    public void StartTimer(int timeArg) {
-        if (!isCountingDown){
+    public void StartTimer(int timeArg = 10) {
+        if (!timerActive){
             HP = MaxHP;
             time = timeArg;
+            timerActive = true;
             isCountingDown = true;
-            _timeText.text = time < 10 ? ("0" + time.ToString()) : time.ToString();
-            Invoke("Tick", 1f);
+            _timeText.text = time < 10 ? ("0" + Mathf.CeilToInt(time).ToString()) : time.ToString();
         }
     }
 
-    void Tick() {
-        if (time > 0 && isCountingDown == true) {
-            time--;
-            _timeText.text = time < 10 ? ("0" + time.ToString()) : time.ToString();
-            Invoke("Tick", 1f);
-        }
-        else {
-            isCountingDown = false;
-            //finished
-            }
-            
+    public void ToggleTimer() {
+        if (isCountingDown) PauseTimer(); else ResumeTimer();
+    }
+
+    public void PauseTimer() {
+        isCountingDown = false;
+    }
+
+    public void ResumeTimer(){
+        isCountingDown = timerActive ? true : false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isCountingDown && HP >= 0)
+        if (isCountingDown && HP > 0 && time > 0)
         {
             HP -= HPchange * Time.deltaTime;
+            time -= Time.deltaTime;
+            int IntTime = Mathf.CeilToInt(time);
+            _timeText.text = IntTime < 10 ? ("0" + IntTime.ToString()) : IntTime.ToString();
         }
-        else if( HP <= 0)
-        {
+        else if (!isCountingDown) {
+        
+        }
+        else{
             isCountingDown = false;
-            //lose
+            timerActive = false;
+            if (HP > 0) {
+                Debug.Log("Time's up");
+            }
+            else {
+                Debug.Log("Lose");
+            }
         }
         _hp.GetComponent<RectTransform>().localScale = new Vector3(HP / MaxHP,1,1);
     }
